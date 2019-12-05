c8bd02163559ee9560ec8522e79e28219ff6a1ca
Mark Nations <45960551+markusj1201@users.noreply.github.com>
2019-12-05 15:15:52 -0600

    Update dex_app.py

--- dex_app.py
+++ dex_app.py
@@ -16,7 +16,7 @@ def main():
 	st.subheader("Datasets For ML Explorer with Streamlit")
 
 	html_temp = """
-	<div style="background-color:tomato;"><p style="color:white;font-size:50px;padding:10px">Streamlit is Awesome</p></div>
+	<div style="background-color:green;"><p style="color:white;font-size:50px;padding:10px">SoHa Change Team Rocks!</p></div>
 	"""
 	st.markdown(html_temp,unsafe_allow_html=True)
 
